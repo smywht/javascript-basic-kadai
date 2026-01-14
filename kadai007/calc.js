e7790b1d6ca783055af27f1d67a9b23d666e1ab0
smywht <hirai.t2@shinmaywa.co.jp>
2026-01-14 17:12:07 +0900

    Add files via upload

---
+++ kadai007/calc.js
@@ -0,0 +1,17 @@
+let num = Math.floor(Math.random() * 100) + 1;
+let x ;
+
+switch ( x ) {
+    case x = num % 15:
+    console.log('3と5の倍数です');
+    break;
+    case x = num % 3 :
+    console.log('3の倍数です');
+    break;
+    case x = num % 5 :
+    console.log('5の倍数です');
+    break;
+    default:
+    console.log(num);
+    break;
+}
